f8d60acdccb0dbde08bc18d196b825a4f642492e
yashraj <yashrajb916@gmail.com>
2026-08-07 18:34:03 +0530

    Add files and changing nature

--- script.js
+++ script.js
@@ -23,10 +23,57 @@ let country = "India";
 weatCont.addEventListener('change', (e) => {
     country = e.target.value;
 })
-EnterBtn.addEventListener('click', () => {
+EnterBtn.addEventListener('click', async() => {
     let cityVal = document.querySelector('#cityName');
     localStorage.setItem('weatherCity',JSON.stringify({"city":cityVal.value,"country":country}))
     let Tval = JSON.parse(localStorage.getItem("weatherCity"));
+
+    weatherContainer.classList.add('afterEnter');
+
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Tval.city},${Tval.country}&appid=33655f13c3abfc64849c907b045bb0d7`);
+    let data = await response.json();
+
+    let weatherImg = "";
+    let weatherID = data.weather[0].id;
+    
+    //cloud image choose 
+    if(weatherID<=232 && weatherID>=200){
+        weatherImg = "./pictures/first.jpg";
+    }else if(weatherID<=321 && weatherID>=300){
+        weatherImg = "./pictures/second.jpg";
+    }else if(weatherID<=531 && weatherID>=500){
+        weatherImg = "./pictures/third.png";
+    }else if(weatherID<=622 && weatherID>=600){
+        weatherImg = "./pictures/fourth.jpg";
+    }else if(weatherID<=781 && weatherID>=700){
+        weatherImg = "./pictures/smoke.jpg";
+    }else if(weatherID===800){
+        weatherImg = "sunny.jpg";
+    }else if(weatherID<=804 && weatherID>=801){
+        weatherImg = "./pictures/cloud.webp";
+    }else{
+        weatherImg = "sunny.jpg";
+    }
+    
+    //weather container decide 
     weatherContainer.innerHTML = `
-    <h2>Country Name:- ${Tval.country}</h2>`;
+    <div class="weatherPic">
+        <img src="${weatherImg}" class="pic" alt="weather">
+        <h2 style="font-size:53px">${Math.ceil(data.main.temp-273)}°C</h2>
+    </div>
+    <div class="weatherOther">
+        <div>
+            <h2>temp in °K</h2>
+            <h3> ${Math.ceil(data.main.temp)}°k </h3>
+        </div>
+        <div>
+            <h2>temp in °F</h2>
+            <h3>${Math.ceil(((data.main.temp-273)*(9/5))+32)}°F</h3>
+        </div>
+        <div>
+            <h2>City:-${Tval.city}</h2>
+            <h2>Country:-${Tval.country}</h2>
+        </div>
+    </div>`;
+
 })
